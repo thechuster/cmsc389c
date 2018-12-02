33865ac1aa9e1f3e78c5f81503fb6152ca79b6cd
Albert Chu <albertchu@wireless-10-105-121-61.umd.edu>
2018-12-01 20:55:23 -0500

    Psuedocode to Code p1

--- MMO_Tok.sol
+++ MMO_Tok.sol
@@ -1,93 +1,90 @@
-pragma solidity ^0.4.20;
+pragma solidity ^0.5.0;
 
 contract MMO_TOK {
-    struct Item {
-        string Attributes;
+    address private Owner;
+    uint private StartingPrice;
+    string private Attributes;
+    uint private Token_Id;
+
+    uint private MaxDuration = 48 hours;
+    uint private MinDuration = 1 hours;
+    uint private AuctionEndTime;
+    bool private EndBool;
+
+    address private Buyer;
+    address private HighestBidder;
+    uint private TotalNumberOfBidders;
+    uint private HighestBid;
+    bool InstantSell;
+
+    address[] BanList = new address[](2^256-1);
+    struct Bidder{
+        address Address;
+        uint NumberOfBids;
     }
 
-    address SellingUser;
-    uint StartingPrice;
-    Item Item;
-
-    address Owner;
-    uint StartingPrice;
-    uint Price;
-    string Attributes;
-    uint Token_Id;
-
-    uint MaxDuration = 24 hours;
-    uint MinDuration = 1 hours;
-    uint AuctionEndTime;
-    bool Ended;
-
-    address HighestBidder;
-    address Bidder;
-    uint HighestBid;
-
-    address Buyer;
-
-    uint Bidders;
-
-    address[] ProhibitedUser = new address[](256);
-
-    constructor (address _SellingUser, uint _StartingPrice, string memory _Attributes, uint _Duration, Item _Item) public {
-        this.SellingUser = _SellingUser;
-        this.StartingPrice = _StartingPrice;
-
-        this.Ended = false;
-        this.Bidders = 0;
-
-        this.Item = _Item;
-        this.Item.Attributes += _Attributes;
+     constructor (address _Owner, uint _StartingPrice, string memory _Attributes, uint _Duration, bool _InstantSell) public {
+        Owner = _Owner;
+        StartingPrice = _StartingPrice;
+        Attributes = _Attributes;
+        EndBool = false;
+        InstantSell = _InstantSell;
+        TotalNumberOfBidders = 0;
+        Bidder[] memory BidderArray = new Bidder[](100);
 
         if (_Duration >= MinDuration && _Duration <= MaxDuration) {
-            this.AuctionEndTime = now + _Duration;
+            AuctionEndTime = now + _Duration;
         } else if (_Duration < MinDuration) {
-            this.AuctionEndTime = now + MinDuration;
+            AuctionEndTime = now + MinDuration;
         } else {
-            this.AuctionEndTime = now + MaxDuration;
+            AuctionEndTime = now + MaxDuration;
         }
-    }
-
-    function CreateBid(address Bidder, uint Bid) public returns (bool) {
-        require(!Ended, "Auction has ended.");
-
+     }
+     // set limit on bids per participant
+     function UpdateBid(address Bidder, uint Bid) public returns (bool) {
+        if(EndBool || TotalNumberOfBidders >= 100) {
+            revert();
+        }
         if (Bid > HighestBid && Bid >= StartingPrice) {
-            if (balanceOf(Bidder) >= Bid) { // correct way to find highest bidder?
                 HighestBidder = Bidder;
                 HighestBid = Bid;
-
-                Bidders += 0;
-
+                TotalNumberOfBidders += 1;
                 return true;
-            }
         }
-
         return false;
     }
 
-    function AuctionEnd() public {
-        require(now >= AuctionEndTime, "Auction hasn't ended yet.");
-        require(!Ended, "This function has not been called.");
-
-        Owner.transfer(Token_Id) to HighestBidder;
-    }
-
-    function Withdraw() public {
-        Ended = true;
-    }
-
-    function Sell() public {
-        Owner.transfer(Token_Id) to Buyer;
-    }
-
-    function DishonestyCheck() public {
-        if Bidder.account <= 0 {
-            assert(false);
+    function UpdateTime(uint NewTime) public {
+        if (AuctionEndTime + NewTime < MaxDuration) {
+            AuctionEndTime = AuctionEndTime + NewTime;
         }
-        else if Bidder.account is in ban list {
-            assert(false);
+        else {
+            AuctionEndTime = MaxDuration;
         }
     }
+    function Sell(address _Buyer) public {
+        if (EndBool) {
+            revert();
+        }
+        Transfer(_Buyer);
+    }
 
+    function Transfer(address _Buyer) public payable {
+
+    }
+
+    // function FindNumberOfBids(address _Bidder) private returns (uint) {
+    //     for (uint i = 0; i < 100; i++) {
+    //         if (BidderArray[i].Address == _Bidder) {
+    //             return Bidder[i].NumberOfBids;
+    //         }
+    //     }
+    // }
+
+    function EndBid() public{
+        EndBool = true;
+    }
+    function AddToBanList(address BannedUser) private {
+        BanList.push(BannedUser);
+    }
 }
