3aac5e0d0c786a7fc5b9fa9de6578b100c2b3747
Albert Chu <albertchu@wireless-10-104-113-207.umd.edu>
2018-12-05 13:50:57 -0500

    Updates

--- MMO_Tok.sol
+++ MMO_Tok.sol
@@ -12,7 +12,6 @@ contract MMO_TOK is ERC721 {
     uint private MinDuration = 1 hours;
     uint private AuctionEndTime;
     bool private EndBool;
-    bool private ConfirmTransaction;
 
     address private Buyer;
     address private HighestBidder;
@@ -20,13 +19,13 @@ contract MMO_TOK is ERC721 {
     uint private HighestBid;
     bool SellBool;
 
-    address[] BanList = new address[](2^256-1);
+    mapping (address => bool) BlockedUsers;
 
     struct Bidder{
         address AddressOfBidder;
         uint NumberOfBids;
     }
-    Bidder[] BidderArray;
+    mapping (address => uint) Bidders;
 
     // Modifier for Owner only functions
     modifier OwnerFunc {
@@ -37,7 +36,7 @@ contract MMO_TOK is ERC721 {
     // Constructor for the contract
      constructor (address _Owner, uint _StartingPrice,
      string memory _Attributes, uint _Duration, bool _SellBool,
-     uint _Token_Id, address[] memory _BanList) public {
+     uint _Token_Id) public {
         Owner = _Owner;
         StartingPrice = _StartingPrice;
         Attributes = _Attributes;
@@ -45,9 +44,6 @@ contract MMO_TOK is ERC721 {
         SellBool = _SellBool;
         TotalNumberOfBidders = 0;
         Token_Id = _Token_Id;
-        BidderArray = new Bidder[](100);
-        BanList = _BanList; // ask if you can do this
-        ConfirmTransaction = false;
 
         if (_Duration >= MinDuration && _Duration <= MaxDuration) {
             AuctionEndTime = now + _Duration;
@@ -84,28 +80,41 @@ contract MMO_TOK is ERC721 {
         return TotalNumberOfBidders;
     }
 
-     // TODO: set limit on bids per participant
-     function UpdateBid(address Bidder, uint Bid) external {
+     // TODO: Add more dishonesty checks
+     function UpdateBid(address _Bidder, uint _Bid) external {
+         CheckAuctionEnd();
         if(EndBool == false || TotalNumberOfBidders >= 100 || SellBool == true) {
             revert();
         }
-        if (Bid > HighestBid && Bid >= StartingPrice) {
-                HighestBidder = Bidder;
-                HighestBid = Bid;
-                TotalNumberOfBidders += 1;
+        if (_Bid > HighestBid && _Bid >= StartingPrice) {
+                HighestBidder = _Bidder;
+                HighestBid = _Bid;
+                if (Bidders[_Bidder] == 0) {
+                    TotalNumberOfBidders += 1;
+                    Bidders[_Bidder] += 1;
+                }
+                else if (Bidders[_Bidder] >= 100) {
+                    AddToBlockList(_Bidder);
+                }
+                else {
+                    Bidders[_Bidder] += 1;
+                }
         }
     }
 
-    // Ask how to do this during office hours
-    function EndAuction() private {
-        ConfirmTransaction = true;
+    // Checks to see if the auction has ended
+    function CheckAuctionEnd() private {
+        if (now == AuctionEndTime){
+            EndBool = true;
+            //TODO: Transfer
+        }
     }
 
     // This allows a bidder to withdraw their bid from the array
     // of bidders and decreases the TotalNumberOfBidders
     function WithdrawBid(address _Bidder) external {
-        require(FindBidder(_Bidder));
-        // TODO: add code to remove bidder from the bidder array
+        Bidders[_Bidder] = 0;
+        BlockedUsers[_Bidder] = true;
         TotalNumberOfBidders--;
     }
 
@@ -124,57 +133,30 @@ contract MMO_TOK is ERC721 {
             revert();
         }
         else if (SellBool) {
-            ConfirmTransaction = true;
             // TODO: Transfer();
         }
     }
 
-    // Checks to see if a bidder is in the array of bidders
-    function FindBidder(address _Bidder) private view returns(bool) {
-        for (uint i = 0; i < 100; i++) {
-            if (BidderArray[i].AddressOfBidder == _Bidder) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     // Finds the number of bids a bidder has made
     function FindNumberOfBids(address _Bidder) private view returns (uint) {
-        require(FindBidder(_Bidder));
-        for (uint i = 0; i < 100; i++) {
-            if (BidderArray[i].AddressOfBidder == _Bidder) {
-                return BidderArray[i].NumberOfBids;
-            }
-        }
+        return Bidders[_Bidder];
     }
 
     //Function for the owner to manually end the bid.
     function EndBidding(bool sell) OwnerFunc external{
         EndBool = true;
         if (sell == true) {
-            ConfirmTransaction = true;
             //TODO: Transfer()
         }
     }
 
-    // Interface function
-    function approve(address _approved, uint256 _tokenId) external payable {
-        emit Approval(Owner, _approved, _tokenId);
-    }
-
-    // Interface transfer function
+    // TODO: IMPLEMENT TRANSFER
     function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
-
         emit Transfer(_from, _to, _tokenId);
     }
 
-
-    // TODO:function Transfer() public payable {
-
-    // }
-
-    function AddToBanList(address BannedUser) private {
-        BanList.push(BannedUser);
+    // Function to add user to BlockedUsers
+    function AddToBlockList(address BannedUser) private {
+        BlockedUsers[BannedUser] = true;
     }
 }
